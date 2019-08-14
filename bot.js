b4de362d67b712b011becbf4e3c34f4de66d59f6
anam4nop <54060600+anam4nop@users.noreply.github.com>
2019-08-14 04:51:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@
 const client = new Discord.Client();
 const client2 = new Discord.Client();
 const myid = ['450093301412921345'];
-const prefix = ['-'];
+const prefix = ['('];
 
 client.on('ready', () => {
    console.log(`----------------`);
